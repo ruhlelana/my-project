6d06b760a90e236315320263ae853e48bef88414
pryspry <halo@pryspry.com>
2019-03-19 00:14:16 +0700

    Commit pry

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>Welcome Prys!</h1>
+    <h1>Welcome Prys dan Ervin!</h1>
     <h2>Yoiiii</h2>
     <p><nuxt-link to="/product">Product page</nuxt-link></p>
     <p><nuxt-link to="/product">Contact page</nuxt-link></p>
